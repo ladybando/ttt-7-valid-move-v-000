2d04e7bb39c34e41292c007a4e8351842518db14
Ayana Bando <ayanaflax@yahoo.com>
2017-10-21 12:22:48 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
     if board[index] ==  0 || 1 || 2 || 3 || 4 || 5|| 6|| 7 || 8
       true
         end
-   if position_taken?
+   if position_taken?(board, index)
      false
  end
 end
